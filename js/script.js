42e537be043c146e8cf2449ea6ba90988dc5c4af
Natalya Zhoholieva <21nataly12@gmail.com>
2016-06-30 22:32:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    var $ul = $('nav ul');
+
+    $('.menu-header').click(function(){
+        $ul.slideToggle(500);
+    });
+
+});
+
+$(function(){
+
+    var $ul = $('.nav ul');
+
+    $('.nav-header').click(function(){
+        $ul.slideToggle(500);
+    });
+
+});
